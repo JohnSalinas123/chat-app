6e55bbf370fc213b02b7133d2f96de2eb14ad72f
JohnSalinas123 <salinasjohn257@gmail.com>
2022-05-27 20:17:02 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+class ChatApp extends React.Component {
+
+    render() {
+
+        return (
+            <div id="outer-chatbox">
+
+                <div className="chatbox">
+
+
+
+                </div>
+                
+            </div>
+        )
+    }
+
+}
+
+function MessageBubble(props) {
+    return (
+        <div className="message-bubble">
+            <p>{this.props.message}</p>
+            <p>{this.props.time}</p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(Cha)
