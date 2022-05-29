faed12145589c79795e3bd98fe49f01474f4b4d6
JohnSalinas123 <salinasjohn257@gmail.com>
2022-05-29 12:00:59 -0700

    add colors, user welcome, input text and button

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
+import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
 class ChatApp extends React.Component {
 
+
+    renderMessage(message,time) {
+
+    }
+
+
     render() {
 
         return (
-            <div id="outer-chatbox">
+            
+            <div className="outer-chatbox">
+                <CurrentUser userName="John Salinas" />
 
                 <div className="chatbox">
 
-
+                
 
                 </div>
-                
+                <ChatInput />
             </div>
         )
     }
 
+}
+
+function CurrentUser(props) {
+
+    return (
+        <div className="current-user-box">
+            <p>Welcome {props.userName}!</p>
+        </div>
+    )
+
+}
+
+function ChatInput() {
+
+    return (
+        <div className="chat-input">
+            <div className="chat-textbox-box">
+                <input className="chat-textbox" type="text"/>
+            </div>
+            <input className="chat-button" type="submit" value="Send" />
+        </div>
+    )
 }
 
 function MessageBubble(props) {
@@ -31,4 +62,4 @@ function MessageBubble(props) {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Cha)
+root.render(<ChatApp />)
