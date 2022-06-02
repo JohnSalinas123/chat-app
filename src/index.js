86ffd9583a218f9f77a9951cf14ee30427b294f1
JohnSalinas123 <salinasjohn257@gmail.com>
2022-06-02 15:28:36 -0700

    add MessageBubble to render messages from user and others

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
+
 
 class ChatApp extends React.Component {
 
@@ -19,8 +21,11 @@ class ChatApp extends React.Component {
                 <CurrentUser userName="John Salinas" />
 
                 <div className="chatbox">
-
-                
+                    <MessageBubble message="Alright its set then!" owner={true} />
+                    <MessageBubble message="Yeah I'm down!" owner={false} />
+                    <MessageBubble message="Hey, is anyone down to play civ 5?
+                    I want to try out that new strategy with the french.
+                    The french weaponry is superior during this new path." owner={true}/>
 
                 </div>
                 <ChatInput />
@@ -53,11 +58,25 @@ function ChatInput() {
 }
 
 function MessageBubble(props) {
+    let dateObj= new Date();
+    let msgTime = dateObj.toLocaleTimeString();
+    let msgDate = dateObj.toLocaleDateString();
+
+    let ownerClass = "msg-user";
+    if (props.owner == false) {
+        ownerClass = "msg-other";
+    }
+
     return (
-        <div className="message-bubble">
-            <p>{this.props.message}</p>
-            <p>{this.props.time}</p>
+        <div className="msg-block">
+            <div className={`msg-general ${ownerClass}`}>
+                <div className="msg-interior">
+                    <p className="msg-text">{props.message}</p>
+                    <p className="msg-time">{`${msgDate}   ${msgTime}`}</p>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
