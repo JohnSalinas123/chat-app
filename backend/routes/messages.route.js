7e61d88d4e96e05c0107e152c9a32d1bd2d83930
JohnSalinas123 <salinasjohn257@gmail.com>
2022-06-29 23:30:33 -0700

    changed to chatRoute

--- backend/routes/messages.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-// Initialize app
-const router = express.Router();
-router.get('/chat', (req, res) => {
-    res.send('chat')
-});
-
-module.exports = router;
