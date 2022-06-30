af64fc8869a8ec1830a00267403e9e2f63efd15b
JohnSalinas123 <salinasjohn257@gmail.com>
2022-06-29 23:26:48 -0700

    working on api/messages routes, add serve of build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
-const cors = require('cors');
-const messages = require('./models/')
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const path = require('path');
+
+const app = express()
+
+app.use('/api/messages', require('./routes/chatRoute'))
+
+
+const indexPath = __dirname + "/../frontend/public"
+const resolvedPath = path.resolve(indexPath)
+console.log(resolvedPath)
+app.use(express.static(path.join(__dirname, "../frontend","build")))
+app.use(express.static(resolvedPath))
+
+app.listen(process.env.PORT, () => console.log(`Server started on ${port}`));
