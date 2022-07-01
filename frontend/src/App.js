d2c6df0c1c564c1a78058859141792b778dbf30f
JohnSalinas123 <salinasjohn257@gmail.com>
2022-07-01 15:03:28 -0700

    add socket.io context setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./styles/general.css"
 import { ChatApp } from "./pages/chat.js"
 import { Login } from "./pages/login.js"
+import { socket, SocketContext } from "./context/socket";
 
 export function App() {
     return (
-        <div className="full-size">
+        <SocketContext.Provider value={socket}>
+            <div className="full-size">
             <Routes>
                <Route path="/" element={<LoginPage />} />
                <Route path="/chat" element={<ChatPage />} />
             </Routes>
-        </div>
+            </div>
+        </SocketContext.Provider>
+        
     )
 }
 
