577623a11fdbcbf047586478b48aef381417fb2e
JohnSalinas123 <salinasjohn257@gmail.com>
2022-07-07 14:32:24 -0700

    changed styling of main App to center login and chat pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./styles/general.css"
-import { ChatApp } from "./pages/chat.js"
-import { Login } from "./pages/login.js"
-import { socket, SocketContext } from "./context/socket";
+import { ChatApp } from "./pages/chatPage/chatApp.js"
+import { Login } from "./pages/loginPage/login.js"
+import { socket, SocketContext } from "./context/socket.js";
 
 export function App() {
     return (
         <SocketContext.Provider value={socket}>
-            <div className="full-size">
-            <Routes>
-               <Route path="/" element={<LoginPage />} />
-               <Route path="/chat" element={<ChatPage />} />
-            </Routes>
+            <div id="root-div">
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/chat" element={<ChatPage />} />
+                </Routes>
             </div>
         </SocketContext.Provider>
         
