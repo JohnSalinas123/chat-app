6f3c0216350330009d29abbc944cea3818cd88f7
JohnSalinas123 <salinasjohn257@gmail.com>
2022-07-08 20:03:23 -0700

    add timestamps to userModel, password encryption wip

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const userSchema = new Schema ({
-    _id: Schema.Types.ObjectId,
     username: String,
-    date: { type: Date, default: Date.now },
-    //messageCount: Number,
-    lastLogin: { type: Date, default: Date.now }
+    password: String
 
-});
+}, 
+{ timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
 
