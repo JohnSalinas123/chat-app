38c48cb758a7f7a3d46d1b5898687a246dfea18b
JohnSalinas123 <salinasjohn257@gmail.com>
2022-07-11 11:20:30 -0700

    add bcrypt hashing pre middleware for User docs

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose')
+const bcrypt = require("bcryptjs")
 const { Schema } = mongoose;
 
 const userSchema = new Schema ({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 
 }, 
 { timestamps: true });
+
+
+// middleware to hash user password on creation/modification
+userSchema.pre('save', function(next) {
+    const user = this
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, (saltError, salt) => {
+            if (saltError) {
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, (hashError, hash) => {
+                    if (hashError) {
+                        return next(hashError)
+                    }
+
+                    user.password = hash
+                    next()
+
+                })
+            }
+        })
+
+
+    } else {
+        return next()
+    }
+
+
+})
+
 
 module.exports = mongoose.model('User', userSchema);
 
