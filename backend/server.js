ebf8fa43f1d0fbf44edd59c915b6ee2be336f67a
JohnSalinas123 <salinasjohn257@gmail.com>
2022-07-29 15:20:26 -0700

    add socket events for login, signup,new message

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dbConnect = require('./database.js');
 const dotenv = require('dotenv').config();
-const userDAO = require('./dao/userDAO.js');
-const MessagesDAO = require('./dao/messagesDAO');
+//const userDAO = require('./dao/userDAO.js');
+const MessageDAO = require('./dao/messageDAO');
+const { Server } = require("socket.io");
 const path = require('path');
 
 dbConnect();
@@ -15,38 +16,75 @@ app.use(express.urlencoded({extended : false}));
 app.use('/api/messages', require('./routes/messageRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
 
-
 const indexPath = __dirname + "/../frontend/public";
 const resolvedPath = path.resolve(indexPath);
 app.use(express.static(path.join(__dirname, "../frontend","build")));
 app.use(express.static(resolvedPath));
 
-
 const http = require('http');
 const server = http.createServer(app)
-const { Server } = require("socket.io");
-const { emit } = require('process');
+
 const io = new Server(server);
 
 // socket.io connection
 io.on('connection', (socket) => {
 
-    socket.on('guest', () => {
-        console.log("A user has connected")
-    })
+    socket.on("guest", () => {
+        console.log("An unknown user has connected");
+    });
 
-    socket.on('signup', (user, callback) => {
+    socket.on("login", (username) => {
+        console.log(`${username} has logged in!`)
+    });
 
-        // create new user
-        userDAO.createUser(user);
-        console.log(user);
-        callback({
-            valid: true
-        });
+    socket.on("signup", (username) =>  {
+        console.log(`${username} has signed up!`);
+    });
+
+    socket.on("message", (message, callback) => {
+
+        const { username, text, date } = message;
+
+        if (!username || !text) {
+            callback({
+                status: "error",
+                info: "Message details missing!"
+            })
+            return;
+        }
+
+        // create message, add it to database
+        const newMessage = {
+            username : username,
+            text : text,
+            date : date,
+
+        }
+        let createdMessage = MessageDAO.createMessage(newMessage);
+        console.log(newMessage);
+
+        if (createdMessage) {
+            // broadcast new message to all connected
+            socket.broadcast.emit("receiving", newMessage)
+
+            callback({
+                status: "ok",
+                message: newMessage,
+            });
+
+        } else {
+            callback({
+                status: "error",
+                info: "Failed to create message!"
+            });
+        }
+
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+
+        console.log('A user disconnected');    
+
     });
 
 });
